2130e205ed0f64122dcada3acb91a1da2ef23db7
hherlyng <hherlyng@hotmail.com>
2025-02-11 17:59:04 -0800

    code sine curve plot for figure, separate mean c file

---
+++ src/plot_mean_concentration.py
@@ -0,0 +1,84 @@
+import ufl
+
+import numpy   as np
+import pandas  as pd
+import dolfinx as dfx
+import adios4dolfinx     as a4d
+import matplotlib.pyplot as plt
+
+from mpi4py       import MPI
+from scipy.io     import loadmat
+from utilities.mesh import create_ventricle_volumes_meshtags
+
+# Set matplotlib properties
+plt.rcParams.update({
+    "text.usetex" : True,
+    "font.family" : "sans-serif",
+    "axes.spines.top" : False,
+    "axes.spines.right" : False
+})
+plt.rcParams["text.latex.preamble"] += "\\usepackage{sfmath}" # Enable sans-serif math font
+
+comm = MPI.COMM_WORLD # MPI Communicator
+gm   = dfx.mesh.GhostMode.shared_facet
+colors = loadmat("../data/data_photoconversion/aggregated_data/colors")['color'] # The colors used for plotting
+
+# Directories
+flow_dir = '../output/flow/checkpoints/'
+transport_dir = '../output/transport/results/'
+
+# Problem version
+model_version = 'C'
+molecule = 'D3'
+tau_version  = 'variable_tau'
+mesh_version = 'original'
+output_dir = f'../output/illustrations/{mesh_version}/{tau_version}/'
+
+# Read mesh
+mesh_input_filename = flow_dir+f"{tau_version}/pressure+{mesh_version}/model_{model_version}/velocity_data_dt=0.02252"
+mesh = a4d.read_mesh(comm=comm, filename=mesh_input_filename, engine="BP4", ghost_mode=gm)
+
+# Create meshtags and calculate ROI volumes
+mt, ROI_tags = create_ventricle_volumes_meshtags(mesh)
+dx = ufl.Measure('dx', domain=mesh, subdomain_data=mt)
+volumes = [comm.allreduce(dfx.fem.assemble_scalar(dfx.fem.form(1*dx(tag))), op=MPI.SUM) for tag in ROI_tags]
+volumes[3] += volumes[2]+volumes[1]+volumes[0] # Add ROI 1, 2, 3 volumes to ROI 4 volume
+
+# Analysis parameters
+c_threshold1 = 0.25 # threshold value used to calculate "time to threshold" in ROIs 1-4
+c_threshold2 = 0.10 # threshold value used to calculate "time to threshold" in ROIs 5-6
+f = 2.22
+dt = 1/f/20
+
+# Plot parameters
+save_fig = 1 # Save figures if set to 1, don't save if set to 0
+lw = 6 # linewidth
+
+# Loop over molecules
+transport_data_filename = transport_dir + \
+    f"{tau_version}/{mesh_version}/log_model_{model_version}_{molecule}_DG1_pressureBC/data/c_hats.npy"
+
+# Load transport data: c_bars = the total concentration in each ROI
+with open(transport_data_filename, "rb") as file: c_bars = np.load(file) 
+
+# Scale the total concentrations in the ROIs by the volume of the respective ROI
+for i in ROI_tags: c_bars[:, i-1] /= volumes[i-1]
+print("c_bar at final time: ", c_bars[-1, :])
+
+# Get the number of timesteps
+num_timesteps = c_bars.shape[0]
+times = dt*np.arange(num_timesteps)
+
+# Plot the figures
+fig, ax = plt.subplots(figsize=([18, 14]))
+
+[ax.plot(times, c_bars[:, i-1], label=f'ROI {i}', color=colors[i-1], linewidth=lw) for i in ROI_tags]
+
+# Add labels
+ax.set_xlabel("Time [s]", fontsize=60, labelpad=25)
+ax.set_ylabel(r"Mean concentration $\overline{c}$ [-]", fontsize=60, labelpad=25)
+ax.tick_params(labelsize=60)
+fig.tight_layout()
+
+if save_fig: fig.savefig(f"{output_dir}/simulations_time_evolution_model{model_version}_{molecule}_no_legend.png")
+plt.show()
---
+++ src/plot_sine_curve.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Set matplotlib properties
+plt.rcParams.update({
+    "font.family" : "Arial",
+    "axes.spines.top" : False,
+    "axes.spines.right" : False
+})
+print(plt.rcParams.keys())
+
+x = np.linspace(0, 1, 101)
+sine_curve = lambda t: np.sin(2*np.pi*t)
+y = sine_curve(x)
+
+fig, ax = plt.subplots(figsize=[16, 7])
+
+ax.plot(x, y, linewidth=5)
+font = {'family': 'Arial',
+        'size'  : 50}
+
+ax.set_xticks([0.0, 0.25, 0.50, 0.75, 1.00])
+ax.set_xticklabels(['0', '25%', '50%', '75%', '100%'], fontdict=font)
+ax.set_yticks([-1.0, 0.0, 1.0])
+ax.set_yticklabels([r'$-A$', '0', r'$A$'], fontdict=font)
+ax.tick_params(axis='both', pad=20)
+
+# Add points with text to plot
+f = 2.22 # Cardiac frequency [Hz]
+T = 1 / f # Cardiac period [s]
+dt = T / 20 # Timestep size
+x1 = 5/20
+x2 = 10/20
+x3 = 15/20
+y1 = sine_curve(x1)
+y2 = sine_curve(x2)
+y3 = sine_curve(x3)
+
+xs = [x1, x2, x3]
+ys = [y1, y2, y3]
+
+[ax.plot(x, y, color='k', marker='o', markersize=15) for x, y in zip(xs, ys)]
+bfont = {'family': 'Arial',
+         'size'  : 50,
+         'fontweight' : 'bold'}
+ax.text(xs[0], ys[0]*1.125, 'f, h', fontdict=bfont)
+ax.text(xs[1]*1.025, ys[1], 'i', fontdict=bfont)
+ax.text(xs[2], ys[2]*0.85, 'j', fontdict=bfont)
+
+fig.tight_layout()
+fig.savefig('../output/illustrations/misc-figure-material/sine_curve.png')
+plt.show()
